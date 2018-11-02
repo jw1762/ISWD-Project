b9d37f2a792592e5b2f226d90010b18d043fc22e
jw1762 <jw1762@txstate.edu>
2018-11-01 21:00:46 -0500

    Silly Merge Lines ruining our code!
    boooo

--- src/classes/QuoteServlet.java
+++ src/classes/QuoteServlet.java
@@ -12,13 +12,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-<<<<<<< HEAD
 import java.sql.Date;
 import java.util.Locale;
 import java.text.SimpleDateFormat; 
 import java.text.ParseException;
-=======
->>>>>>> refs/remotes/origin/jcwBranch
 import classes.Quote;
 import java.sql.*;
 import com.mysql.*;
@@ -39,8 +36,7 @@ public class QuoteServlet extends HttpServlet
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
 	{		
 		Quote test = new Quote();
-<<<<<<< HEAD
-		
+
 		//int cid = request.getParameter("clientID")
 		//The assignment says nothing about a client ID, 
 		//but it exists in the Quote object. Ignoring for now.
@@ -53,9 +49,6 @@ public class QuoteServlet extends HttpServlet
 		java.sql.Date date = new java.sql.Date(utilDate.getTime());
 		System.out.println(date);
 		
-=======
-			
->>>>>>> refs/remotes/origin/jcwBranch
 		String name = request.getParameter("DelCPN");
 		System.out.println(name);
 		
@@ -86,13 +79,10 @@ public class QuoteServlet extends HttpServlet
 		test.setsuggestedPrice(price);
 		test.setTotalAmountDue(total);
 						
-<<<<<<< HEAD
 		//sets data for JSP file		
 		request.getSession().setAttribute("DelDate", date);	
-=======
 	//sets data for JSP file		
 	//	request.getSession().setAttribute("DelDate", date);	
->>>>>>> refs/remotes/origin/jcwBranch
 		request.getSession().setAttribute("DelLoc", loc);	
 		request.getSession().setAttribute("GalReq", gals);	
 		request.getSession().setAttribute("DelCPN", name);	
@@ -100,7 +90,6 @@ public class QuoteServlet extends HttpServlet
 		request.getSession().setAttribute("DelCPP", phone);	
 		request.getSession().setAttribute("PPG", price);	
 		request.getSession().setAttribute("TotalDue", total);	
-<<<<<<< HEAD
 		
 		///////////////Will not write to disk. Otherwise should work.////////////////////
 	/*	String fileContent = "{";
@@ -122,14 +111,10 @@ public class QuoteServlet extends HttpServlet
 	    writer.close();		*/
 		///////////////////////////////////////////////////////////////////////////
 				
-=======
-			
 	//Calls new DatabaseCon class, containing functions for various DB actions.
 		DatabaseCon querydb = new DatabaseCon();
 		querydb.getQuoteHistory();
 	//
-		
->>>>>>> refs/remotes/origin/jcwBranch
 		//Add the new quote object to the 
 		//list arraylist of quotes.
 		QuoteViewer qv = new QuoteViewer();
